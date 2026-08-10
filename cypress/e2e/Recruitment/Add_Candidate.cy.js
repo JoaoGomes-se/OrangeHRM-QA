5814bf087c541d82e0cb175ea944ebee1aa090b3
JônatasP <persjonatas@gmail.com>
2026-08-10 10:51:27 -0300

    feat: novos testes para automatização de fluxos em Add_Candidate

--- cypress/e2e/Recruitment/Add_Candidate.cy.js
+++ cypress/e2e/Recruitment/Add_Candidate.cy.js
@@ -18,5 +18,25 @@ describe('Recruitment - Add Candidate', () => {
       cy.contains('button', 'Save').click();
   });
 
+    it('Verifica o limite de caracteres para o campo Contact Number', () => {
+      cy.contains('button', /Add/i).click();
+      cy.get('.oxd-grid-3 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
+        .clear()
+        .type('999999999999999999999999999999999999999999');
+      cy.get(':nth-child(3) > .oxd-grid-3 > :nth-child(2) > .oxd-input-group').should('be.visible');
+  });
+   it('Verifica a obrigatoriedade do campo First Name', () => {
+    cy.contains('button', /Add/i).click();
+    cy.get('[name="firstName"]').clear();
+    cy.contains('button', 'Save').click();
+    cy.get('.oxd-input-group > .oxd-text').should('contain.text', 'Required');
+  });
+  it('Verifica a obrigatoriedade do campo Last Name', () => {
+    cy.contains('button', /Add/i).click();
+    cy.get('[name="lastName"]').clear();
+    cy.contains('button', 'Save').click();
+    cy.get('.oxd-input-group > .oxd-text').should('contain.text', 'Required');
+  });
+
 
 });
